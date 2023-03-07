8bf1383f8272d22792b8dab955b2c5c9af8f6d69
AtaeTL <ataetlidii@gmail.com>
2023-03-07 10:54:13 +0100

    Update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-"""
-Prints the ASCII alphabet in lowercase.
-"""
-
-for i in range(ord('a'), ord('z')+1):
-    print("{:c}".format(i), end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end='')
