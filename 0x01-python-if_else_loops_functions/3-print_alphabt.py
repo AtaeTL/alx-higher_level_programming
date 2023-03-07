4e2ba85ce6dbe3d1189ee4369977be5b1be5ec86
AtaeTL <ataetlidii@gmail.com>
2023-03-07 10:30:07 +0100

    ok

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-for letter in range(ord('a'), ord('z')+1):
-    if chr(letter) not in "qe":
-        print("{}".format(chr(letter)), end="")
-print()
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end='')
