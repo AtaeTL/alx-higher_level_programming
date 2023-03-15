0e3fb1cc45137e19e93b34d8c234b556c247ee8b
AtaeTL <ataetlidii@gmail.com>
2023-03-15 11:04:08 +0100

    Update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
+    if not isinstance(roman_string, str):
         return 0
-
-    roman_int = 0
-    roman_numeral_map = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40,
-                         'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
-    i = 0
-    while i < len(roman_string):
-        if i + 1 < len(roman_string) and roman_numeral_map.get(roman_string[i]) < roman_numeral_map.get(
-                roman_string[i + 1]):
-            roman_int += roman_numeral_map[roman_string[i + 1]] - roman_numeral_map[roman_string[i]]
-            i += 2
-        else:
-            roman_int += roman_numeral_map[roman_string[i]]
-            i += 1
-    return roman_int
+    total = 0
+    num = 0
+    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for r in reversed(roman_string):
+        num = digits[r]
+        total += num if total < num * 5 else -num
+    return total
