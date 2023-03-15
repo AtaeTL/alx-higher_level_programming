e3f404bfdc135286dd1bab5fac1f119640a27a1a
AtaeTL <ataetlidii@gmail.com>
2023-03-15 10:55:34 +0100

    Update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,45 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    # Fail checks, none, not a string
-    if not roman_string:
+    if not roman_string or not isinstance(roman_string, str):
         return 0
-    if not isinstance(roman_string, str):
-        return 0
-    if not roman_string.isupper():
-        return 0
-    # Dictionary for roman numerals
-    r_dict = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
 
-    result = 0
-    temp = list(roman_string)
-    # Concat 4 and 9s
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'V':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            idx += 1
-    # Search in dict for correct numbers and add
-    for k, v in r_dict.items():
-        for index in temp:
-            if index == k:
-                result += v
-    return result
+    roman_int = 0
+    roman_numeral_map = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40,
+                         'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
+    i = 0
+    while i < len(roman_string):
+        if i + 1 < len(roman_string) and roman_numeral_map.get(roman_string[i]) < roman_numeral_map.get(
+                roman_string[i + 1]):
+            roman_int += roman_numeral_map[roman_string[i + 1]] - roman_numeral_map[roman_string[i]]
+            i += 2
+        else:
+            roman_int += roman_numeral_map[roman_string[i]]
+            i += 1
+    return roman_int
